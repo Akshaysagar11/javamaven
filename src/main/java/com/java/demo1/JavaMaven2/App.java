c3f9d67f9d1ffab83b26e55faeb928c651428915
Akshay <akshaygunti19@gmail.com>
2024-11-14 15:52:47 +0530

    added folders

---
+++ src/main/java/com/java/demo1/JavaMaven2/App.java
@@ -0,0 +1,14 @@
+package com.java.demo1.JavaMaven2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Akshay" );
+        System.out.println( "22bd1a054j" );
+    }
+}
